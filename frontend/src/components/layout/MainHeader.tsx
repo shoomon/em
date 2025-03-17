129a7ca1091c0ad02524eecb1d09c072f599587d
박창조 <ckdwh1202@naver.com>
2025-03-17 23:22:05 +0900

    [FE] feat: MainHeader 컴포넌트 개발
    S12P21A407-100

--- frontend/src/components/layout/MainHeader.tsx
+++ frontend/src/components/layout/MainHeader.tsx
@@ -1,7 +1,20 @@
+import { BellIcon } from "lucide-react"
+import { Link } from "react-router-dom"
+
+import logo from "@/assets/em_logo.svg"
+
 const MainHeader = () => {
+  // TODO : 알림이 있을 경우 UI 변경
   return (
-    <header className="flex items-center justify-between">
-      <div>mainHeader</div>
+    <header className="flex items-center justify-between h-full px-5 border-b border-em-gray-sm">
+      <div className="flex items-center justify-center h-full cursor-pointer">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-full" />
+        </Link>
+      </div>
+      <div className="flex items-center justify-center h-full cursor-pointer">
+        <BellIcon className="size-6 text-em-gray" fill="currentColor" />
+      </div>
     </header>
   )
 }
