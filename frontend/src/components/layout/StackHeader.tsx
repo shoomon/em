bc1e0a244aed3736a84aa70da49a670487f3573d
박창조 <ckdwh1202@naver.com>
2025-03-18 00:14:03 +0900

    [FE] feat: StackHeader 컴포넌트 개발
    S12P21A407-102
    
    논의 사항
    - 헤더 타이틀을 zustand로 관리하는 것에 대하여 논의

--- frontend/src/components/layout/StackHeader.tsx
+++ frontend/src/components/layout/StackHeader.tsx
@@ -1,7 +1,26 @@
+import { ChevronLeftIcon } from "lucide-react"
+import { useNavigate } from "react-router-dom"
+
 const StackHeader = () => {
+  // Todo: Header 타이틀을 zustand로 관리하는 것에 대하여 논의
+  const title = "여기는 무엇이 좋을꼬"
+
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate(-1)
+  }
+
   return (
-    <header className="flex items-center justify-between">
-      <div>StackHeader</div>
+    <header className="flex items-center justify-between h-full px-5">
+      <div onClick={handleClick} className="cursor-pointer">
+        <ChevronLeftIcon className="w-8 h-8" />
+      </div>
+
+      <div>
+        <h3 className="text-xl font-bold text-em-black">{title}</h3>
+      </div>
+      <div></div>
     </header>
   )
 }
