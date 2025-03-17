e6b4039f9d38e63a29fbc5014f1d4832ff6c8b09
박창조 <ckdwh1202@naver.com>
2025-03-18 00:44:34 +0900

    [FE] feat: useDevice 훅 추가
    - 모바일 디바이스 및 운영체제(IOS) 확인 훅 추가
    - useDevice 훅 적용  : BaseLayout, MainLayout

--- frontend/src/layout/BaseLayout.tsx
+++ frontend/src/layout/BaseLayout.tsx
@@ -1,12 +1,15 @@
 import type { ReactNode } from "react"
 
+import useDevice from "@/hooks/useDevice"
 interface BaseLayoutProps {
   children: ReactNode
 }
 
 const BaseLayout = ({ children }: BaseLayoutProps) => {
+  const { isMobile } = useDevice()
   return (
-    <div className="flex relative flex-col min-h-screen max-h-screen max-w-[600px] mx-auto">
+    <div
+      className={`flex relative flex-col min-h-screen max-w-[600px] mx-auto ${!isMobile ? "border-x border-em-gray-sm" : ""}`}>
       {children}
     </div>
   )
