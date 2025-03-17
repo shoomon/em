6c6377ea34289764df470a0449eba60208fc9b5c
박창조 <ckdwh1202@naver.com>
2025-03-17 23:45:09 +0900

    [FE] feat: MainNavigationBar 컴포넌트 개발
    S12P21A407-101

--- frontend/src/components/layout/MainNavigationBar.tsx
+++ frontend/src/components/layout/MainNavigationBar.tsx
@@ -1,7 +1,34 @@
+import { HomeIcon, UserIcon } from "lucide-react"
+import { useCallback } from "react"
+import { useLocation } from "react-router-dom"
+
+import NavigationBarItem from "@/components/layout/NavigationBarItem"
+
 const MainNavigationBar = () => {
+  const { pathname } = useLocation()
+
+  const navigationItems = [
+    {
+      id: 1,
+      name: "홈",
+      icon: <HomeIcon />,
+      path: "/",
+    },
+    {
+      id: 2,
+      name: "마이페이지",
+      icon: <UserIcon />,
+      path: "/mypage",
+    },
+  ]
+
+  const isActive = useCallback((path: string) => pathname === path, [pathname])
+
   return (
-    <nav className="flex items-center justify-between">
-      <div>MainNavigationBar</div>
+    <nav className="flex items-center w-full h-full border-t border-em-gray-sm">
+      {navigationItems.map((data) => (
+        <NavigationBarItem key={data.id} data={data} isActive={isActive(data.path)} />
+      ))}
     </nav>
   )
 }
---
+++ frontend/src/components/layout/NavigationBarItem.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from "react"
+import { Link } from "react-router-dom"
+
+interface NavigationBarItemProps {
+  data: {
+    id: number
+    name: string
+    icon: ReactNode
+    path: string
+  }
+  isActive: boolean
+}
+
+const NavigationBarItem = ({ data, isActive }: NavigationBarItemProps) => {
+  const { id, path, name, icon } = data
+
+  return (
+    <div className="flex-1" key={id}>
+      <Link to={path} key={id}>
+        <div className="flex flex-col items-center justify-center w-full gap-1">
+          <span className={`size-6 ${isActive ? "text-em-black" : "text-em-gray"} `}>{icon}</span>
+          <span className={`text-xs ${isActive ? "text-em-black" : "text-em-gray"} `}>{name}</span>
+        </div>
+      </Link>
+    </div>
+  )
+}
+export default NavigationBarItem
