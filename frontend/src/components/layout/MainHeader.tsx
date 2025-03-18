2a7c37b48a885ab76ab21cc535b9f78825d69768
박창조 <ckdwhd1202@gmail.com>
2025-03-18 12:06:43 +0900

    [FE] refactor: MainHeader 마크업 수정

--- frontend/src/components/layout/MainHeader.tsx
+++ frontend/src/components/layout/MainHeader.tsx
@@ -7,11 +7,9 @@ const MainHeader = () => {
   // TODO : 알림이 있을 경우 UI 변경
   return (
     <header className="flex items-center justify-between h-full px-5 border-b border-em-gray-sm">
-      <div className="flex items-center justify-center h-full cursor-pointer">
-        <Link to="/" className="flex items-center justify-center w-full h-full">
-          <img src={logo} alt="logo" className="w-full" />
-        </Link>
-      </div>
+      <Link to="/" className="flex items-center justify-center h-full cursor-pointer">
+        <img src={logo} alt="logo" className="w-full" />
+      </Link>
       <div className="flex items-center justify-center h-full px-3 cursor-pointer">
         <BellIcon className="size-6 text-em-gray" fill="currentColor" />
       </div>
