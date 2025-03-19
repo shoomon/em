f84ea1a5199bd7124b3a34c4e2c3dcc10d23e51b
엄예림 <djadpfa1002@naver.com>
2025-03-19 12:56:08 +0900

    [BE] feat: 회원 entity, repository 생성 (S12P21A407-114)

---
+++ backend-spring/src/main/java/com/ssafy/em/user/domain/User.java
@@ -0,0 +1,70 @@
+package com.ssafy.em.user.domain;
+
+import com.ssafy.em.auth.domain.entity.Provider;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Provider provider;  // 예: KAKAO
+
+    @Column(name = "social_id", length = 100, unique = true)
+    private String socialId;
+
+    @Column(name = "username", length = 50)
+    private String username;
+
+    @Column(name = "profile_image_url", length = 500)
+    private String profileImageUrl;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Builder
+    public User(Provider provider, String socialId, String username, String profileImageUrl) {
+        this.provider = provider;
+        this.socialId = socialId;
+        this.username = username;
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public User update(String username, String profileImageUrl) {
+        this.username = username;
+        this.profileImageUrl = profileImageUrl;
+        return this;
+    }
+
+}
+
