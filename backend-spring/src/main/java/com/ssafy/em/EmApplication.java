b0a1123331a78ed1c6c5ec4bef3ee904db520f40
엄예림 <djadpfa1002@naver.com>
2025-03-19 12:58:07 +0900

    [BE] feat: JWT properties 생성 (S12P21A407-114)

--- backend-spring/src/main/java/com/ssafy/em/EmApplication.java
+++ backend-spring/src/main/java/com/ssafy/em/EmApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.em;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class EmApplication {
 
     public static void main(String[] args) {
