09f8f0e68fbc13a920c4380f9cb188a78b798cd8
엄예림 <djadpfa1002@naver.com>
2025-03-24 08:50:43 +0900

    [BE] feat: 회원 로그아웃 기능 구현 (S12P21A407-133)

--- backend-spring/src/main/java/com/ssafy/em/common/config/security/SecurityConfig.java
+++ backend-spring/src/main/java/com/ssafy/em/common/config/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.ssafy.em.common.config.security;
 
 import com.ssafy.em.auth.jwt.filter.JwtAuthenticationFilter;
+import com.ssafy.em.auth.jwt.handler.OAuth2LogoutHandler;
 import com.ssafy.em.auth.jwt.handler.OAuth2SuccessHandler;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +19,13 @@ import org.springframework.web.cors.CorsConfigurationSource;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private static final String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
+
     private final CorsConfigurationSource corsConfigurationSource;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final DefaultOAuth2UserService oAuth2UserService;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final OAuth2LogoutHandler oAuth2LogoutHandler;
     private final ExceptionHandlerConfigurer exceptionHandlerConfigurer;
 
     @Bean
@@ -30,17 +35,23 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                                // Swagger나 API Docs는 공개
-                                .requestMatchers("/", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
-                                // OAuth2 로그인 플로우에 필요한 경로도 공개
-                                .requestMatchers("/oauth2/**", "/login/oauth2/**").permitAll()
-                                // 그 외 나머지는 인증 필요
-                                .anyRequest().authenticated()
-                        )
+                        // Swagger나 API Docs는 공개
+                        .requestMatchers("/", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        // OAuth2 로그인 플로우에 필요한 경로도 공개
+                        .requestMatchers("/oauth2/**", "/login/oauth2/**").permitAll()
+                        // 그 외 나머지는 인증 필요
+                        .anyRequest().authenticated()
+                )
                 .formLogin(AbstractHttpConfigurer::disable)
                 .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint(endpoint -> endpoint.userService(oAuth2UserService))
                         .successHandler(oAuth2SuccessHandler)
+                )
+                .logout(logout -> logout
+                        .addLogoutHandler(oAuth2LogoutHandler)
+                        .logoutUrl("/api/auth/logout")
+                        .deleteCookies("REFRESH_TOKEN_COOKIE_NAME")
+                        .logoutSuccessHandler((request, response, authentication) -> response.setStatus(HttpServletResponse.SC_OK))
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exceptionHandlerConfigurer);
