5869101528d89b4eb36fedb0185348cfd81535a7
엄예림 <djadpfa1002@naver.com>
2025-03-24 17:53:08 +0900

    [BE] chore: Swagger 설정 파일 생성 (S12P21A407-150)

---
+++ backend-spring/src/main/java/com/ssafy/em/common/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.ssafy.em.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "Authorization";
+
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("all-api")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("이음(EM) - 지금 여기 감정의 순간을 기록하다")
+                .version("v1.0.0")
+                .description("이음(EM) API 명세서.");
+
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(SECURITY_SCHEME_NAME);
+
+        return new OpenAPI()
+                .info(info)
+                .components(new Components().addSecuritySchemes(SECURITY_SCHEME_NAME, bearerAuth))
+                .addSecurityItem(securityRequirement);
+    }
+}
