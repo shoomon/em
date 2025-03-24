03b531de8be9df6f561e78631aeabe7cbdbc2a8e
엄예림 <djadpfa1002@naver.com>
2025-03-24 18:03:07 +0900

    [BE] feat: 회원 토큰 재발급 endpoint 설정 및 Swagger 적용 (S12P21A407-134)

--- backend-spring/src/main/java/com/ssafy/em/common/config/security/SecurityConfig.java
+++ backend-spring/src/main/java/com/ssafy/em/common/config/security/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                         // Swagger나 API Docs는 공개
                         .requestMatchers("/", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         // OAuth2 로그인 플로우에 필요한 경로도 공개
-                        .requestMatchers("/oauth2/**", "/login/oauth2/**").permitAll()
+                        .requestMatchers("/oauth2/**", "/login/oauth2/**", "/auth/reissue", "/error").permitAll()
                         // 그 외 나머지는 인증 필요
                         .anyRequest().authenticated()
                 )
