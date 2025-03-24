642d8f0fd78ff21dd8ee62f5dfcf46018f1b15f8
엄예림 <djadpfa1002@naver.com>
2025-03-24 17:59:26 +0900

    [BE] feat: AuthErrorCode에 만료된 JWT에 대한 code 생성 및 JwtProvider에 커스텀 error code 반영 (S12P21A407-134)

--- backend-spring/src/main/java/com/ssafy/em/auth/exception/AuthErrorCode.java
+++ backend-spring/src/main/java/com/ssafy/em/auth/exception/AuthErrorCode.java
@@ -1,11 +1,15 @@
 package com.ssafy.em.auth.exception;
 
+import com.ssafy.em.common.exception.ErrorCode;
 import lombok.Getter;
 
 @Getter
 public enum AuthErrorCode {
-    UNAUTHORIZED("B4011", "인증이 필요합니다."),
+    // 401
+    UNAUTHORIZED("B4011", "인증에 실패했습니다."),
+    EXPIRED_JWT_TOKEN("B4012", "만료된 JWT 토큰입니다."),
 
+    // 403
     FORBIDDEN("B4031", "접근 권한이 없습니다.");
 
     private final String code;
@@ -14,5 +18,9 @@ public enum AuthErrorCode {
     AuthErrorCode(String code, String message) {
         this.code = code;
         this.message = message;
+    }
+
+    public ErrorCode toErrorCode() {
+        return new ErrorCode(code, message);
     }
 }
