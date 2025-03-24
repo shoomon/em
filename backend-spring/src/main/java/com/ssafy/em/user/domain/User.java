b09a3b5719be123efe96ed5a45783304bd5615d7
엄예림 <djadpfa1002@naver.com>
2025-03-24 14:40:43 +0900

    [BE] fix: PostgreSQL의 Enum 타입과 Hibernate가 정확히 매핑되도록 수정 (S12P21A407-134)

--- backend-spring/src/main/java/com/ssafy/em/user/domain/User.java
+++ backend-spring/src/main/java/com/ssafy/em/user/domain/User.java
@@ -4,8 +4,6 @@ import com.ssafy.em.auth.domain.entity.Provider;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -33,9 +31,8 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "provider", nullable = false, columnDefinition = "provider_type")
     @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Column(name = "provider", nullable = false, columnDefinition = "provider")
     private Provider provider;  // 예: KAKAO
 
     @Column(name = "social_id", length = 100, unique = true)
