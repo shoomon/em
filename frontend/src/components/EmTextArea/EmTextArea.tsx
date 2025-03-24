46356a2dba7101f739e543447c24cc07cb477e3d
박창조 <ckdwhd1202@gmail.com>
2025-03-24 14:37:14 +0900

    [FE] refactor: TextArea 상태 관리 로직 변경 및 length 최적화
    S12P21A407-127

--- frontend/src/components/EmTextArea/EmTextArea.tsx
+++ frontend/src/components/EmTextArea/EmTextArea.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, TextareaHTMLAttributes, useState } from "react"
+import { ChangeEvent, TextareaHTMLAttributes, useRef } from "react"
 
 import { cn } from "@/utils/cn"
 
@@ -6,37 +6,51 @@ interface EmTextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   className?: string
   placeholder?: string
   maxLength?: number
+  text?: string // 텍스트 상태 전달
+  onTextChange?: (_text: string) => void // 텍스트 상태 변경
 }
 
 const EmTextArea = ({
   className,
   placeholder = "이곳에 텍스트를 입력해 주세요.",
   maxLength = 500,
+  text,
+  onTextChange,
   ...props
 }: EmTextAreaProps) => {
-  const [text, setText] = useState("")
-
-  const isOverMaxLength = text.length >= maxLength
+  const textRef = useRef<HTMLTextAreaElement>(null)
+  const countRef = useRef<HTMLSpanElement>(null)
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const { value } = e.target
-    setText(value)
-    if (value.length > maxLength) {
-      setText(value.slice(0, maxLength))
-    }
-  }
+    const { length } = value
 
+    // 최대 길이 체크
+    if (countRef.current) {
+      if (length > maxLength) {
+        countRef.current.classList.add("text-em-anger")
+      } else {
+        countRef.current.classList.remove("text-em-anger")
+      }
+      countRef.current.textContent = length.toString()
+    }
+
+    // 텍스트 상태 변경
+    onTextChange?.(value)
+  }
   return (
     <div className="w-full h-full bg-em-white flex flex-col gap-2 p-4 border border-em-gray-md rounded-xl">
       <textarea
-        onChange={handleChange}
+        ref={textRef}
         value={text}
+        onChange={handleChange}
+        maxLength={maxLength}
         className={cn("outline-none resize-none w-full h-full min-h-60", className)}
         {...props}
         placeholder={placeholder}
       />
       <p className="flex justify-end items-center gap-2 text-sm text-em-gray">
-        <span className={cn({ "text-em-anger": isOverMaxLength })}>{text.length}</span>
+        <span ref={countRef}>0</span>
         <span className="">/ {maxLength}</span>
       </p>
     </div>
