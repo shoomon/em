ab56055fc7df0febe53a28c14ef6a986242604c0
박민경 <hogik1027@naver.com>
2025-03-25 13:56:12 +0900

    [FE] feat: 프로필 카드 컴포넌트 UserProfileCard 구현
    S12P21A407-117

---
+++ frontend/src/features/profile/components/UserProfileCard.tsx
@@ -0,0 +1,35 @@
+import useProfileData from "../hooks/useProfileData"
+
+const UserProfileCard = () => {
+  const { profile, isLoading, error } = useProfileData()
+
+  if (isLoading) {
+    return <div>로딩 중...</div>
+  }
+  if (error) {
+    return <div>{error}</div>
+  }
+
+  if (!profile) {
+    return <div>프로필 정보가 없습니다.</div>
+  }
+
+  return (
+    <div className="flex items-center gap-4 bg-em-white">
+      <img
+        src={profile.profileImageUrl || "https://cataas.com/cat"}
+        alt="프로필 사진"
+        className="w-12 h-12 bg-gray-300 rounded-full"
+      />
+      <div className="flex flex-col justify-center gap-1">
+        <h2 className="text-xl font-bold text-em-black">{profile.nickname}</h2>
+        <div className="flex items-center gap-1">
+          <img src={profile.providerImage} alt="소셜 아이콘" className="w-4 h-4" />
+          <p className="text-xs font-semibold text-em-black">{profile.socialId}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default UserProfileCard
