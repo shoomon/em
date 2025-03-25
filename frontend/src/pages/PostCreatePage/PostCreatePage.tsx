6b96a9d39b1dae19c87b55d9515cb12889d64f6f
박창조 <ckdwhd1202@gmail.com>
2025-03-26 08:52:20 +0900

    [FE] feat: zustand를 통해 StackHeader title 관리
    S12P21A407-125

--- frontend/src/pages/PostCreatePage/PostCreatePage.tsx
+++ frontend/src/pages/PostCreatePage/PostCreatePage.tsx
@@ -1,6 +1,13 @@
 import PostCreateForm from "@/features/post/components/PostCreateForm"
+import useStackLayoutStore from "@/store/useStackLayoutStore"
+import { useEffect } from "react"
 
 const PostCreatePage = () => {
+  const setTitle = useStackLayoutStore((state) => state.setTitle)
+
+  useEffect(() => {
+    setTitle("게시글 작성")
+  }, [])
   return (
     <div className="h-full">
       <PostCreateForm />
