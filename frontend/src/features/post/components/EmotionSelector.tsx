2748e08bb95fc9e2056a5b7403243aa00b757b07
박창조 <ckdwhd1202@gmail.com>
2025-03-25 18:28:06 +0900

    [FE] feat: 감정 선택 컴포넌트 추가
    S12P21A407-125
    S12P21A407-120
    
    - 감정 선택 컴포넌트 추가
    - 감정 선택 컴포넌트에 대한 타입 정의
    - 감정 선택 컴포넌트에 대한 상수 정의
    - 포스트 작성 페이지에 감정 선택 컴포넌트 추가

---
+++ frontend/src/features/post/components/EmotionSelector.tsx
@@ -0,0 +1,34 @@
+import EmSection from "@/components/EmSection/EmSection"
+import EmotionSelectItem from "@/features/emotion/components/EmotionSelectItem"
+import { EMOTION_ITEMS } from "@/features/emotion/constants"
+
+type EmotionSelectorProps = {
+  onEmotionChange: (_emotion: string) => void
+  emotionState: string
+}
+
+const EmotionSelector = ({ onEmotionChange, emotionState }: EmotionSelectorProps) => {
+  const handleEmotionSelect = (emotionId: string) => {
+    onEmotionChange(emotionId)
+  }
+
+  return (
+    <EmSection>
+      <EmSection.Header title="감정 선택" description="당신의 감정은 어떠신가요?" />
+      <div className="space-y-4">
+        <div className="grid grid-cols-2 sm:grid-cols-4 gap-3">
+          {EMOTION_ITEMS.map((emotion) => (
+            <EmotionSelectItem
+              key={emotion.id}
+              onSelect={handleEmotionSelect}
+              isSelected={emotionState === emotion.id}
+              emotion={emotion}
+            />
+          ))}
+        </div>
+      </div>
+    </EmSection>
+  )
+}
+
+export default EmotionSelector
