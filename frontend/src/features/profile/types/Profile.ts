3780c0f3690014946b6b9d1f591560de171bb169
박민경 <hogik1027@naver.com>
2025-03-26 10:21:13 +0900

    [FE] fix: 마이페이지에서 잘못된 필드명 사용 수정 (nickname → username)
    S12P21A407-167

--- frontend/src/features/profile/types/Profile.ts
+++ frontend/src/features/profile/types/Profile.ts
@@ -3,7 +3,7 @@ import { OAuthProvider } from "@/features/auth/types/auth.type"
 export interface Profile {
   userId: number
   profileImageUrl: string
-  nickname: string
+  username: string
   provider: OAuthProvider
   socialId: string
 }
