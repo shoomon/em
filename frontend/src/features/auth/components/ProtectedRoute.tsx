bcc29a479f597814d4df19e01041c230672b2b52
박창조 <ckdwhd1202@gmail.com>
2025-03-27 13:16:25 +0900

    [FE] refactor: ProtectedRoute에 체크 로직 추가

--- frontend/src/features/auth/components/ProtectedRoute.tsx
+++ frontend/src/features/auth/components/ProtectedRoute.tsx
@@ -1,9 +1,19 @@
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
 import { jwtDecode } from "jwt-decode"
 import { Navigate, Outlet } from "react-router-dom"
 
 const ProtectedRoute = () => {
   // 토큰 조회
   const accessToken = localStorage.getItem("accessToken")
+
+  const { isError } = useQuery({
+    queryKey: ["isLoggedIn"],
+    queryFn: () => {
+      return axios.get("/user")
+    },
+    enabled: !!accessToken,
+  })
 
   // 로그인 상태
   // const { isLoggedIn, setIsLoggedIn } = useAuthStore()
@@ -26,7 +36,7 @@ const ProtectedRoute = () => {
   }
 
   // 토큰이 없거나 유효하지 않으면 로그인 페이지로 리다이렉트
-  if (!accessToken || !isTokenValid) {
+  if (!accessToken || !isTokenValid || isError) {
     return <Navigate to="/login" replace />
   }
 
