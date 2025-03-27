f9bd261e28a1fb1fb4f3ecc51c044bee3915c5cb
박창조 <ckdwhd1202@gmail.com>
2025-03-27 15:18:40 +0900

    [FE] refactor: 글 작성 중 뒤로가기 누를 시 경고창 추가
    S12P21A407-179

--- frontend/src/pages/PostCreatePage/PostCreatePage.tsx
+++ frontend/src/pages/PostCreatePage/PostCreatePage.tsx
@@ -1,17 +1,31 @@
 import PostCreateForm from "@/features/post/components/PostCreateForm"
 import useStackLayoutStore from "@/store/useStackLayoutStore"
 import { useEffect } from "react"
+import { useLocation } from "react-router-dom"
 
 const PostCreatePage = () => {
+  const location = useLocation()
   const setTitle = useStackLayoutStore((state) => state.setTitle)
 
   useEffect(() => {
     setTitle("게시글 작성")
+  }, [])
+
+  // 현재 페이지에서 벗어날 때 타이틀 초기화
+  useEffect(() => {
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      e.preventDefault()
+      if (confirm("게시글 작성을 취소하시겠습니까?")) {
+        setTitle("")
+      }
+    }
+
+    window.addEventListener("beforeunload", handleBeforeUnload)
 
     return () => {
-      setTitle("")
+      window.removeEventListener("beforeunload", handleBeforeUnload)
     }
-  }, [])
+  }, [location])
 
   return (
     <div className="w-full flex-1 flex justify-center items-center">
