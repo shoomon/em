8c06d0af54f130362e4842e39924653e58edc51a
박창조 <ckdwhd1202@gmail.com>
2025-03-31 13:06:22 +0900

    [FE] hotfix: PWA 제거

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig, loadEnv } from "vite"
-import { VitePWA } from "vite-plugin-pwa"
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -13,23 +12,23 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       tailwindcss(),
-      VitePWA({
-        registerType: "autoUpdate",
-        includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
-        manifest: {
-          name: "이음(EM)",
-          short_name: "이음",
-          description: "지금 여기, 감정의 순간을 기록하다",
-          theme_color: "#fafafa",
-          // icons: [
-          //   {
-          //     src: "/icons/icon-192x192.png",
-          //     sizes: "192x192",
-          //     type: "image/png",
-          //   },
-          // ],
-        },
-      }),
+      // VitePWA({
+      //   registerType: "autoUpdate",
+      //   includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      //   manifest: {
+      //     name: "이음(EM)",
+      //     short_name: "이음",
+      //     description: "지금 여기, 감정의 순간을 기록하다",
+      //     theme_color: "#fafafa",
+      //     // icons: [
+      //     //   {
+      //     //     src: "/icons/icon-192x192.png",
+      //     //     sizes: "192x192",
+      //     //     type: "image/png",
+      //     //   },
+      //     // ],
+      //   },
+      // }),
     ],
     resolve: {
       alias: {
