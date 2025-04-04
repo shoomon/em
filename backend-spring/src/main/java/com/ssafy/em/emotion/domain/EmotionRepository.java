544ea936f719df1fe06625acd65f43824f406bf7
엄예림 <djadpfa1002@naver.com>
2025-04-04 15:12:58 +0900

    [BE] fix: 감정 조회 시 isActive한 감정만 조회될 수 있도록 수정 (S12P21A407-272)

--- backend-spring/src/main/java/com/ssafy/em/emotion/domain/EmotionRepository.java
+++ backend-spring/src/main/java/com/ssafy/em/emotion/domain/EmotionRepository.java
@@ -3,9 +3,14 @@ package com.ssafy.em.emotion.domain;
 import com.ssafy.em.emotion.domain.entity.Emotion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EmotionRepository extends JpaRepository<Emotion, Integer> {
 
     Optional<Emotion> findByNameAndIsActiveTrue(String name);
+
+    List<Emotion> findByIsActiveTrueOrderByIdAsc();
+
+    Optional<Emotion> findByIdAndIsActiveTrue(Integer id);
 }
