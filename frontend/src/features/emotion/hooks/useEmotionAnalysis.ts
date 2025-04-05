351293130f17bda0a1e3ef75f24446222f1531f1
박창조 <ckdwh1202@gmail.com>
2025-04-06 02:36:01 +0900

    [FE] feat: AI 감정 분석 기능 API 연결 및 로딩 애니메이션 추가
    S12P21A407-168
    
    - 감정 분석 기능 추가
    - API 빠른 응답으로 인한 지연 시간 추가

--- frontend/src/features/emotion/hooks/useEmotionAnalysis.ts
+++ frontend/src/features/emotion/hooks/useEmotionAnalysis.ts
@@ -4,6 +4,13 @@ import { fetchEmotionAnalysis } from "../api/emotion"
 const useEmotionAnalysis = (content: string) => {
   const mutate = useMutation({
     mutationFn: () => fetchEmotionAnalysis(content),
+    onSuccess: async (data) => {
+      await new Promise((resolve) => setTimeout(resolve, 1500))
+      return data
+    },
+    onError: (error) => {
+      console.log("실패", error)
+    },
   })
 
   return mutate
