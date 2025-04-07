ebd29117aba85199c7905bf8b8775fedce641ccc
박창조 <ckdwh1202@gmail.com>
2025-04-08 02:36:20 +0900

    [FE] chore: 빌드시 이미지 압축 추가
    S12P21A407-301

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig, loadEnv } from "vite"
 import { compression } from "vite-plugin-compression2"
+import imagemin from "vite-plugin-imagemin"
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -18,9 +19,28 @@ export default defineConfig(({ mode }) => {
         compressionOptions: {
           level: 9, // 최대 압축 수준
         },
+        include: ["**/*.{js,css,html}"],
       }),
       compression({
         algorithm: "brotliCompress",
+        include: ["**/*.{js,css,html}"],
+      }),
+      imagemin({
+        gifsicle: false,
+        optipng: {
+          optimizationLevel: 3,
+        },
+        webp: {
+          quality: 80,
+        },
+        svgo: {
+          plugins: [
+            { name: "removeViewBox", active: false },
+            { name: "removeEmptyAttrs", active: false },
+            { name: "cleanupIDs", active: false },
+            { name: "collapseGroups", active: false },
+          ],
+        },
       }),
       // VitePWA({
       //   registerType: "autoUpdate",
