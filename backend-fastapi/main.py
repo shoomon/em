67828daff47c40445d478b00ff1aec7df7695811
배승호 <moomu.like@gmail.com>
2025-04-07 14:02:36 +0900

    [AI] feat: ã로그인 미들웨어 작성

--- backend-fastapi/main.py
+++ backend-fastapi/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+from fastapi.security import HTTPBearer
 
+from src.common.middleware.LoginMiddleware import loginMiddleware
 from src.core.startUpEvent import startUpEvent
 from src.curse_detection.presentation.CurseDetectionController import curseDetectionController
 from src.dummy.presentation.DummyController import dummyController
@@ -8,6 +11,32 @@ from src.music_recommendation.presentation.MusicRecommendationController import 
 from src.emotion_detection.presentation.EmotionDetectionController import emotionDetectionController
 app = FastAPI()
 app.add_event_handler("startup", startUpEvent)
+app.middleware("http")(loginMiddleware)
+
+security = HTTPBearer()
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="이음",
+        version="1.0.0",
+        description="Swagger with Bearer Token",
+        routes=app.routes,
+    )
+    openapi_schema["components"]["securitySchemes"] = {
+        "bearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",  # 생략 가능
+        }
+    }
+    for path in openapi_schema["paths"].values():
+        for method in path.values():
+            method.setdefault("security", []).append({"bearerAuth": []})
+    app.openapi_schema = openapi_schema
+    return openapi_schema
+
+app.openapi = custom_openapi
 app.include_router(dummyController)
 app.include_router(musicRecommendationController)
 app.include_router(emotionDetectionController)
