ab3e9cb016a6b5159f32b9ac563c512db5512eb7
박창조 <ckdwhd1202@gmail.com>
2025-04-09 13:05:42 +0900

    [FE] hotfix: marker icon 경로 수정

--- frontend/src/features/map/constants.ts
+++ frontend/src/features/map/constants.ts
@@ -1,22 +1,28 @@
+const bgImage = "/assets/heart.svg"
+
+const markerContents = `
+    <div class="flex items-center justify-center animate-bounce text-sm opacity-65 text-em-white size-8 font-semibold"
+    style="background:url(${bgImage});background-size:contain;"></div>`
+
 const htmlClusterMarkers = [
   {
-    content: `<div class="flex items-center justify-center animate-bounce text-sm opacity-65 text-em-white size-8 font-semibold"style="background:url(/src/assets/heart.svg);background-size:contain;"></div>`,
+    content: markerContents,
     anchor: new window.naver.maps.Point(16, 16),
   },
   {
-    content: `<div class="flex items-center justify-center animate-bounce text-sm opacity-65 text-em-white size-9 font-semibold"style="background:url(/src/assets/heart.svg);background-size:contain"></div>`,
+    content: markerContents,
     anchor: new window.naver.maps.Point(18, 18),
   },
   {
-    content: `<div class="flex items-center justify-center animate-bounce text-sm opacity-65 text-em-white size-10 font-semibold"style="background:url(/src/assets/heart.svg);background-size:contain"></div>`,
+    content: markerContents,
     anchor: new window.naver.maps.Point(20, 20),
   },
   {
-    content: `<div class="flex items-center justify-center animate-bounce text-sm opacity-65 text-em-white size-11 font-semibold"style="background:url(/src/assets/heart.svg);background-size:contain"></div>`,
+    content: markerContents,
     anchor: new window.naver.maps.Point(22, 22),
   },
   {
-    content: `<div class="flex items-center justify-center animate-bounce text-sm opacity-65 text-em-white size-12 font-semibold"style="background:url(/src/assets/heart.svg);background-size:contain"></div>`,
+    content: markerContents,
     anchor: new window.naver.maps.Point(24, 24),
   },
 ]
