2d9117fc1b214ba64e2b138950dd0ecda4062ba2
전종우 <jinlaove17@naver.com>
2025-04-09 10:13:51 +0900

    [FE] design: 텍스트 굵기 조정

--- frontend/src/features/history/components/MyPostList.tsx
+++ frontend/src/features/history/components/MyPostList.tsx
@@ -14,8 +14,8 @@ const MyPostList = ({ postList, onDeletePost }: MyPostListProps) => {
       <div className="flex flex-col flex-1 gap-4">
         {postList.length === 0 ? (
           <div className="flex flex-col gap-3 p-4 rounded-lg bg-em-gray-md">
-            <div className="text-[clamp(0.8rem,4vw,1.2rem)] text-em-black">
-              작성한 게시글이 없어요
+            <div className="text-[clamp(1rem,4vw,1.4rem)] text-em-black/70 font-bold">
+              작성한 게시글이 없어요..
             </div>
             <img src={emptyImage} alt="" className="self-end w-1/2 my-4" />
           </div>
